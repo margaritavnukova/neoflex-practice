2d6cb58026f7966984f5d09949bcea6cf42aa470
Маргарита Внукова <margaritavnukova82@gmail.com>
2024-05-06 17:02:18 +0300

    Added title and description to the UI

--- src/main/java/ru/neoflex/practice/PracticeApplication.java
+++ src/main/java/ru/neoflex/practice/PracticeApplication.java
@@ -1,8 +1,11 @@
 package ru.neoflex.practice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(info = @Info(title = "Calculator API", version = "1.0", description = "Summing and subtraction"))
 @SpringBootApplication
 public class PracticeApplication {
 
